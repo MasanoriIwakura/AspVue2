c4b27e6f17dc86c9fe956638c2b55d8a4549dd6d
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-07-22 23:29:26 +0900

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'client/src/main.ts',
+      template: 'client/public/index.html',
+      filename: 'index.html',
+      title: 'AspVue2'
+    }
+  }
+}
