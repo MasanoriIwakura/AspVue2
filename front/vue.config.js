6f2855515e7f31380e871c57efc349770b592c9f
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-07-24 19:27:59 +0900

    Update directories
    APIとフロントエンドを分割

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   pages: {
     index: {
-      entry: 'client/src/main.js',
-      template: 'client/public/index.html',
+      entry: 'src/main.js',
+      template: 'public/index.html',
       filename: 'index.html',
       title: 'AspVue2'
     }
   },
-  outputDir: 'wwwroot'
+  outputDir: 'dist'
 }
